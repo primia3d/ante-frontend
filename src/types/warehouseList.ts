742a2f5b5d51b17ea200472d01c15ac53e76443c
Tin <legarde.234837@fairview.sti.edu.ph>
2024-06-13 00:13:43 +0800

    feat:integrate view list of warehouse

---
+++ src/types/warehouseList.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+import { warehouseFormSchema } from '@/schema/warehouseSchema';
+export type warehouseFormSchema = z.infer<typeof warehouseFormSchema>;
+
+export type TViewWarehouse = {
+  id: string;
+  name: string;
+  location: string; 
+  size: number;
+};
